5b17af5708c8fcabae6441cebbfbc11a1427b8ed
prazhenik.dev <prazhenik.dev@gmail.com>
2022-01-18 17:15:38 +0200

    hero video

--- #src/js/local/functions.js
+++ #src/js/local/functions.js
@@ -1,20 +1,3 @@
-// ---------- удаление смещения контента при показе\скрытии верхней панели моб браузера -------------
-
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-
-
-// ------- удаление смещения контента при показе\скрытии верхней панели моб браузера после в альб. ориент ---------
-
-// // We listen to the resize event
-// window.addEventListener('resize', () => {
-// 	// We execute the same script as before
-// 	let vh = window.innerHeight * 0.01;
-// 	document.documentElement.style.setProperty('--vh', `${vh}px`);
-// });
 
 
 // --------------------- проверка типа устройства ------------------------
